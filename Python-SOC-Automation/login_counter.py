579f77003ede3734b7d3e408b3ff265bfcf66f7e
alexopelayo-arch <alexopelayo@gmail.com>
2026-05-08 13:32:41 -0600

    Update login_counter.py
    Basic login attempt counter. 
    Simulates monitoring failed access attempts and triggers 
    a security lockout once the limit is reached.

--- Python-SOC-Automation/login_counter.py
+++ Python-SOC-Automation/login_counter.py
@@ -1,1 +1,18 @@
+# Contador de Intentos de Acceso
+# Este script simula el conteo de intentos fallidos de un usuario
 
+intentos_fallidos = 0
+limite_seguridad = 5
+
+print("=== Monitoreando intentos de acceso ===")
+
+# Simulamos 6 intentos
+for i in range(1, 7):
+    intentos_fallidos = i
+    print(f"Intento numero {intentos_fallidos} registrado.")
+    
+    if intentos_fallidos >= limite_seguridad:
+        print("[BLOQUEO] Se ha alcanzado el limite de seguridad.")
+        break # Detiene el conteo al llegar al limite
+
+print("\nProceso de monitoreo finalizado.")
